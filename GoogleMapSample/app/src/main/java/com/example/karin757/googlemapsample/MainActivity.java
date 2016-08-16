c2e1236bbf20a84282da38abbfda30fc5cf56343
mizuyabu <b3176@oic.jp>
2016-08-16 12:46:27 +0900

    GoogleMapの表示

--- GoogleMapSample/app/src/main/java/com/example/karin757/googlemapsample/MainActivity.java
+++ GoogleMapSample/app/src/main/java/com/example/karin757/googlemapsample/MainActivity.java
@@ -1,13 +1,27 @@
 package com.example.karin757.googlemapsample;
 
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapFragment;
 
+import com.google.android.gms.maps.MapFragment;
+
+public class MainActivity extends AppCompatActivity {
+    MapFragment mf;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mf = MapFragment.newInstance();
+
+        FragmentManager fm = getFragmentManager();
+        FragmentTransaction ft = fm.beginTransaction();
+        ft.add(android.R.id.content, mf);
+        ft.commit();
     }
 }
